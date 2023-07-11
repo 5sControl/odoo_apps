71d6b8aa5d6936b502e4c108719f74a276bf42d5
anikeenko <dimskay1988@icloud.com>
2023-07-11 16:56:19 +0300

    added models works

--- master_detail/models/models.py
+++ master_detail/models/models.py
@@ -28,6 +28,22 @@ class Act(models.Model):
         ('no_record', 'Missing entry')
     ]
 
+    name = fields.Char(string='Act Number')
+    description = fields.Text(string='Description')
+    contract_id = fields.Many2one('contract.contract', string='Contract')
+    address = fields.Char(string='Address')
+    master_id = fields.Many2one('hr.employee', string='Master')
+    phone_number = fields.Text(string='Phone Number')
+    date_receipt_work = fields.Date(string='Date of receipt of work')
+    preliminary_dismantling = fields.Integer(string='Ardomas preliminarus dangų plotas')
+    permission_excavate = fields.Selection(selection=PERMISSION_SELECTION, string='Permission to excavate')
+    work_ids = fields.One2many('contract.work', 'act_id', string='Works')
+
+
+class Work(models.Model):
+    _name = 'contract.work'
+    _description = 'Repair Works'
+
     REPAIR_WORKS_SELECTION = [
         ('1', 'Trūkimo vietos užtaisymas, kai gylis iki 2,0 m., plieninio vamzdžio skersmuo'),
         ('2', 'Trūkimo vietos užtaisymas, kai gylis iki 2,5 m., plieninio vamzdžio skersmuo'),
@@ -36,12 +52,29 @@ class Act(models.Model):
         ('5', 'Trūkimo vietos užtaisymas, kai gylis iki 5,0 m., plieninio vamzdžio skersmuo'),
         ('6', 'Trūkimo vietos užtaisymas, kai gylis iki 6,0 m., plieninio vamzdžio skersmuo'),
         ('7', 'Vamzdžio movos sandarinimas, kai gylis iki 2,0 m., ketinio vamdžio skersmuo, mm'),
-        ('8', 'Vamzdžio movos sandarinimas, kai gylis iki 2,5 m., ketinio vamdžio skersmuo, mm')
+        ('8', 'Vamzdžio movos sandarinimas, kai gylis iki 2,5 m., ketinio vamdžio skersmuo, mm'),
+        ('9', 'Vamzdžio movos sandarinimas, kai gylis iki 3,0 m., ketinio vamdžio skersmuo, mm'),
+        ('10', 'Vamzdžio movos sandarinimas, kai gylis iki 4,0 m., ketinio vamdžio skersmuo, mm'),
+        ('11', 'Remontinės sudedamos movos uždėjimas, kai gylis iki 2,0 m. ketinio vamzdžio skersmuo, mm'),
+        ('12', 'Remontinės sudedamos movos uždėjimas, kai gylis iki 2,5 m. ketinio vamzdžio skersmuo, mm'),
+        ('13', 'Remontinės sudedamos movos uždėjimas, kai gylis iki 3,0 m. ketinio vamzdžio skersmuo, mm'),
+        ('14', 'Remontinės sudedamos movos uždėjimas, kai gylis iki 4,0 m. ketinio vamzdžio skersmuo, mm'),
+        ('15', 'Vamzdžio pažeistos dalies keitimas, kai gylis iki 2,0 m., ketinio vamdžio skersmuo, mm'),
+        ('16', 'Vamzdžio pažeistos dalies keitimas, kai gylis iki 2,5 m., ketinio vamdžio skersmuo, mm'),
+        ('17', 'Vamzdžio pažeistos dalies keitimas, kai gylis iki 3,0 m., ketinio vamdžio skersmuo, mm'),
+        ('18', 'Vamzdžio pažeistos dalies keitimas, kai gylis iki 4,0 m., ketinio vamdžio skersmuo, mm'),
+        ('19', 'Vamzdžio pažeistos dalies keitimas, kai gylis iki 2,0 m, polietileninio vamdžio skersmuo, mm'),
+        ('20', 'Vamzdžio pažeistos dalies keitimas, kai gylis iki 2,5 m, polietileninio vamdžio skersmuo, mm'),
     ]
 
     diameter_selection = [
+        ('32', '32'),
+        ('40', '40'),
+        ('50', '50'),
+        ('63', '63'),
         ('80', '80'),
         ('100', '100'),
+        ('110', '110'),
         ('125', '125'),
         ('150', '150'),
         ('200', '200'),
@@ -55,25 +88,17 @@ class Act(models.Model):
         ('800', '800'),
     ]
 
-    name = fields.Char(string='Act Number')
     description = fields.Text(string='Description')
-    contract_id = fields.Many2one('contract.contract', string='Contract')
-    photos = fields.Many2many('ir.attachment', string='Photos')
-    address = fields.Char(string='Address')
-    master_id = fields.Many2one('hr.employee', string='Master')
     quantity = fields.Float(string='Quantity')
+    coverage_type = fields.Char(string='Coverage Type')
     unit = fields.Char(string='Unit of measurement')
     unit_price = fields.Float(string='Unit Price')
+    # photos = fields.Many2many('ir.attachment', string='Photos')
+    # employers_ids = fields.Many2many('hr.employee', string='Employees', relation='act_employers_rel')
     start_date = fields.Date(string='Start Date')
     end_date = fields.Date(string='End Date')
-    date_receipt_work = fields.Date(string='Date of receipt of work')
-    coverage_type = fields.Char(string='Coverage Type')
-    preliminary_dismantling = fields.Integer(string='Ardomas preliminarus dangų plotas')
-    employers_ids = fields.Many2many('hr.employee', string='Employees', relation='act_employers_rel')
-    phone_number = fields.Text(string='Phone Number')
+    act_id = fields.Many2one('contract.act', string='Act')
     precipitation_date = fields.Date(string='Precipitation Date')
     description_precipitation = fields.Text(string='Description of precipitation')
-    permission_excavate = fields.Selection(selection=PERMISSION_SELECTION, string='Permission to excavate')
     diameter = fields.Selection(selection=diameter_selection, string='Pipe Diameter ⌀')
     repair_works = fields.Selection(selection=REPAIR_WORKS_SELECTION, string='Repair Works')
-
