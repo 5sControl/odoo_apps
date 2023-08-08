699be94446f60d0b4a326b593eca96b20ac957c1
anikeenko <dimskay1988@icloud.com>
2023-08-08 16:55:23 +0300

    edit models, views

--- min_max_5s/models/models.py
+++ min_max_5s/models/models.py
@@ -1,5 +1,6 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
+import requests
+
+from odoo import models, fields, api
 
 
 class ExternalServiceConnection(models.Model):
@@ -20,3 +21,19 @@ class Items(models.Model):
     object_type = fields.Char(string='Object Type')
     status = fields.Char(string='Status')
     current_stock_level = fields.Integer(string='Current Stock Level')
+
+    @api.model
+    def update_items_from_api(self):
+        url = "https://40da-134-17-26-206.ngrok-free.app/api/inventory/items/"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            items_data = response.json()
+
+            for item_data in items_data:
+                self.create({
+                    'name': item_data.get('name'),
+                    'object_type': item_data.get('object_type'),
+                    'status': item_data.get('status'),
+                    'current_stock_level': item_data.get('current_stock_level')
+                })
