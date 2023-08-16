ec4b418ce1c1e0511e00fd04da221a26968edfaa
anikeenko <dimskay1988@icloud.com>
2023-08-16 19:04:32 +0300

    ADDED sending a notification to the chat

--- min_max_5s/controllers/controllers.py
+++ min_max_5s/controllers/controllers.py
@@ -21,3 +21,34 @@ class MinMaxController(http.Controller):
     @http.route('/min_max/ping', type='http', auth='public')
     def ping(self):
         return json.dumps({'success': True})
+
+    @http.route('/min_max/send_message', type='http', auth='public', methods=['POST'], csrf=False)
+    def send_message(self):
+        body = request.httprequest.data
+        data = json.loads(body)
+        message = data.get('message', '')
+        print(message)
+
+        user_5controlS = request.env['res.users'].sudo().search([('login', '=', '5controlS')], limit=1)
+
+        if user_5controlS:
+            admin_user = request.env['res.users'].sudo().search([('login', '=', 'admin')], limit=1)
+            if admin_user:
+                channel = request.env['mail.channel'].sudo().search([
+                    ('channel_partner_ids', 'in', [user_5controlS.partner_id.id, admin_user.partner_id.id]),
+                    ('channel_type', '=', 'chat')
+                ], limit=1)
+                if not channel:
+                    channel = request.env['mail.channel'].sudo().create({
+                        'channel_partner_ids': [(6, 0, [user_5controlS.partner_id.id, admin_user.partner_id.id])],
+                        'public': 'private',
+                        'channel_type': 'chat',
+                    })
+                channel.sudo().message_post(body=message, author_id=user_5controlS.partner_id.id,
+                                            message_type="comment")
+                return json.dumps({'success': True})
+        return json.dumps({'success': False})
+
+
+
+
