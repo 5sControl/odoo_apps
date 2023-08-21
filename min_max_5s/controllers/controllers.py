69e7c4c13e5ebd18492ee71a83b19c19637a84d5
anikeenko <dimskay1988@icloud.com>
2023-08-21 14:53:52 +0300

    EDIT message

--- min_max_5s/controllers/controllers.py
+++ min_max_5s/controllers/controllers.py
@@ -29,7 +29,8 @@ class MinMaxController(http.Controller):
         # body = request.httprequest.data
         # print("data", request.data)
         # data = json.loads(body)
-        message = request.form.get('message', '')
+        data = json.loads(request.httprequest.data.decode('utf-8'))
+        message = data.get('message', '')
         last_connection = request.env['min_max.connection'].sudo().search([], order='id desc', limit=1)
 
         for user in last_connection.notification_users:
